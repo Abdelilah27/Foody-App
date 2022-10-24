2c56846ed9622032df3c327fd8dfe2a2b12b7b86
Abdelilah <abdelilah.ngadi27@gmail.com>
2022-10-24 19:18:32 +0100

    Splash Screen Implementation

--- app/src/main/java/com/foody/foody/MainActivity.kt
+++ app/src/main/java/com/foody/foody/MainActivity.kt
@@ -2,10 +2,20 @@ package com.foody.foody
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        lateinit var navController: NavController
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
